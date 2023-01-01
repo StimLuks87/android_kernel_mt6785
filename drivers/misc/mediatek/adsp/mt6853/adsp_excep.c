4e6a9268078e42e5df1e388c2d33ab0bf26fa324
LinkBoi00 <linkdevel@protonmail.com>
2023-01-01 21:21:06 +0200

    drivers: misc/mtk: adsp: Silence debug and verbose logging
    Change the log level of the messages to the proper ones
    
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    (cherry picked from commit 203f3151f41d543e6ff76056de1acdb3899b55df)

--- drivers/misc/mediatek/adsp/mt6853/adsp_excep.c
+++ drivers/misc/mediatek/adsp/mt6853/adsp_excep.c
@@ -1,3 +1,4 @@
+
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
  * Copyright (c) 2020 MediaTek Inc.
@@ -195,7 +196,7 @@ static void adsp_exception_dump(struct adsp_exception_control *ctrl)
 	if (dump_flag) {
 		ret = dump_buffer(ctrl, coredump_id);
 		if (ret < 0)
-			pr_info("%s, excep dump fail ret(%d)", __func__, ret);
+			pr_debug("%s, excep dump fail ret(%d)", __func__, ret);
 	}
 
 	n += snprintf(detail + n, ADSP_AED_STR_LEN - n, "%s %s\n",
@@ -212,7 +213,7 @@ static void adsp_exception_dump(struct adsp_exception_control *ctrl)
 		n += snprintf(detail + n, ADSP_AED_STR_LEN - n, "%s",
 			      coredump->assert_log);
 	}
-	pr_info("%s", detail);
+	pr_debug("%s", detail);
 
 	/* adsp aed api, only detail information available*/
 	aed_common_exception_api("adsp", (const int *)coredump, coredump_size,
@@ -561,4 +562,3 @@ struct attribute_group adsp_excep_attr_group = {
 	.attrs = adsp_excep_attrs,
 	.bin_attrs = adsp_excep_bin_attrs,
 };
-
